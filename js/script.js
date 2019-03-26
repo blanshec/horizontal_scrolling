ed15c52795f49a541051288ac7a0675413d0ee88
alinaadburshina <alinaabdurshina@gmail.com>
2019-03-27 00:50:11 +0300

    smooth scrolling done js scripts to be implemented

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const menu_link = document.getElementsByClassName("menu_link");
+const page = document.getElementsByClassName("page")[0];
+const menu = document.getElementsByClassName("menu__link");
+const section = document.getElementsByClassName("section");
 
-meun_link[3].addEventListener("click", function() {
-    document.querySelector('#tables').scrollIntoView({ 
-        behavior: 'smooth' 
-      });
-})
+section[0].addEventListener("mouseenter", function(event) {
+  let currentId = event.target.id;
+  console.log(currentId);
+
+  let currentPage = menu.indexOf(event.target);
+  console.log(currentPage);
+});
